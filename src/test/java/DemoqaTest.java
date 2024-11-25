96cf338df7930f9459a4f8638c6dc84a162091f6
olganow <olganow@yandex.ru>
2024-11-25 22:30:32 +0300

    fix after review

--- src/test/java/DemoqaTest.java
+++ src/test/java/DemoqaTest.java
@@ -1,6 +1,5 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
